5c5900252fbfc0ba142fbde82fbefbe405f72b1f
Musab Momin <musab.momin@kapso.in>
2022-09-01 16:06:11 +0530

    winner logic is done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Board from './components/Board';
+import { calculateWinner } from './helpers';
 import './styles/root.scss';
 
 
 function App() {
+
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [isXNext, setIsXNext] = useState(true); // to take a track of whether the player is X or O
+
+  const winner = calculateWinner(board)
+
+  const changeBoard = (position)=>{
+
+    // if there is already value present or we have winner so don't make change in state
+    if(board[position] || winner)
+      return;
+
+    // changing the state of at perticular index value
+    setBoard( prev =>{
+      return prev.map((square, pos)=>{
+        // if the position is same than change the value 
+        if(pos === position)
+          return isXNext ? "X" : "O"
+      
+        return square
+      })
+    });
+    setIsXNext(!isXNext)
+  }
+
   return(
     <main className='app'>
         <h1>Tic Tac Toe</h1>
-        <Board />
+        <p>{ winner ? `winner is ${winner}` : `${isXNext ? "Next player is X" : "Next player is O"}` }</p>
+        <Board board={ board } changeBoard={ changeBoard }  />
     </main>
   );
 }
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,30 +1,9 @@
-import React, {useState} from 'react'
+import React from 'react'
 import Square from './Square'
 
-const Board = () => {
+const Board = ({ board, changeBoard }) => {
 
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isXNext, setIsXNext] = useState(true); // to take a track of whether the player is X or O
-
-
-  const changeBoard = (position)=>{
-
-    // if there is already value present
-    if(board[position])
-      return;
-
-    // changing the state of at perticular index value
-    setBoard( prev =>{
-      return prev.map((square, pos)=>{
-        // if the position is same than change the value 
-        if(pos === position)
-          return isXNext ? "X" : "O"
-      
-        return square
-      })
-    });
-    setIsXNext(!isXNext)
-  }
+ 
 
   const constructBoard = (position)=>{
     return <Square value={board[position]} changeBoard={()=>changeBoard(position)}/> 
