eee80dca7cec17cd9955b56f33e9b3739e457116
Musab Momin <musab.momin@kapso.in>
2022-09-01 14:24:54 +0530

    sass is installed and root styling is done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import Board from './components/Board';
+import './styles/root.scss';
+
 
 function App() {
-  return <div>App</div>;
+  return(
+    <main className='app'>
+        <h1>Tic Tac Toe</h1>
+        <Board />
+    </main>
+  );
 }
 
 export default App;
